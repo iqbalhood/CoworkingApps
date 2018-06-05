562b70c9643b107736e6821a596bca37a3f9dcd6
iqbalhood <iqbalhood@gmail.com>
2018-06-06 00:53:55 +0700

    initial push code on android native

---
+++ app/src/main/java/com/iqbalhood/coworking/MainActivity.java
@@ -0,0 +1,13 @@
+package com.iqbalhood.coworking;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
